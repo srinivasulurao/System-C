8ef8478f5aecab39f8c8dcd3c702835cb23dd050
Rao <SRao7@ad.harman.com>
2020-06-01 17:57:44 +0530

    isNan fixed

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -1,12 +1,12 @@
 function valueChangeHandler(value){
   var donation_field=document.getElementById('donation_value');  
   var error_notifier=document.getElementById('error_notifier');
-  if((parseInt(value)>0) && Number(value) != NaN){ 
-       donation_field.style.border='1px solid #ccc';  
-       error_notifier.innerHTML="";  
+  if(isNaN(value)){ 
+     donation_field.style.border='1px solid red';
+     error_notifier.innerHTML="Please enter a valid donation value"; 
   }
   else{
-       donation_field.style.border='1px solid red';
-       error_notifier.innerHTML="Please enter a valid donation value";
+     donation_field.style.border='1px solid #ccc';  
+     error_notifier.innerHTML="";  
   }
 }
